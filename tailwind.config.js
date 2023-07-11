ca965008c5807dd107ce66fa6954e6f70dbb0727
Bagus TRIANURDIN <btstargroup@gmail.com>
2023-07-12 02:38:15 +0700

    feat(package.json): install some library

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,7 @@
 /** @type {import('tailwindcss').Config} */
 const defaultTheme = require('tailwindcss/defaultTheme');
 module.exports = {
+  important: ['data-table-library_table'],
   content: [
     './pages/**/*.{js,ts,jsx,tsx,mdx}',
     './components/**/*.{js,ts,jsx,tsx,mdx}',
