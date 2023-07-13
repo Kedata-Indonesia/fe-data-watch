75f6e19a341b66ef288e6c7c0b4fb98358674936
Friandy D. Noviandha <freyandhy@gmail.com>
2023-07-13 08:02:57 +0700

    feat(hotjar): add hotjar tracking (#15)

--- pages/_app.js
+++ pages/_app.js
@@ -1,23 +1,25 @@
 import '@/styles/globals.css';
-import Head from 'next/head';
+import { useEffect } from 'react';
 import { queryClient } from '@/services/libs/react-query';
 import { QueryClientProvider } from '@tanstack/react-query';
 import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
 import { GoogleAnalytics } from 'nextjs-google-analytics';
 import { DefaultSeo } from 'next-seo';
+import { hotjar } from 'react-hotjar';
 import SEO_CONFIG from '@/next-seo.config';
 
 export default function App({ Component, pageProps }) {
   const getLayout = Component.getLayout || (page => page);
 
+  useEffect(() => {
+    hotjar.initialize(process.env.NEXT_PUBLIC_HJID, process.env.NEXT_PUBLIC_HJSV);
+  }, []);
+
   return (
     <QueryClientProvider client={queryClient}>
-      {/* <Head>
-        <title>Empowering Data Analysis with Free - Data Watch</title>
-      </Head> */}
       <GoogleAnalytics trackPageViews={{ ignoreHashChange: true }} />
-      {getLayout(<Component {...pageProps} />)}
       <DefaultSeo {...SEO_CONFIG} />
+      {getLayout(<Component {...pageProps} />)}
       <ReactQueryDevtools initialIsOpen={false} />
     </QueryClientProvider>
   );
