79b90f7b0cfb1cb95d9f3c595cce2418d573e68e
Bagus TRIANURDIN <btstargroup@gmail.com>
2023-07-28 11:00:53 +0700

    feat(pages/_app): add toast & hydrate component

--- pages/_app.js
+++ pages/_app.js
@@ -1,24 +1,28 @@
 import '@/styles/globals.css';
 import Head from 'next/head';
 import { queryClient } from '@/services/libs/react-query';
-import { QueryClientProvider } from '@tanstack/react-query';
+import { Hydrate, QueryClientProvider } from '@tanstack/react-query';
 import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
 import { GoogleAnalytics } from 'nextjs-google-analytics';
 import { DefaultSeo } from 'next-seo';
 import SEO_CONFIG from '@/next-seo.config';
+import { Toaster } from 'react-hot-toast';
 
 export default function App({ Component, pageProps }) {
   const getLayout = Component.getLayout || (page => page);
 
   return (
     <QueryClientProvider client={queryClient}>
-      {/* <Head>
+      <Hydrate state={pageProps.dehydratedState}>
+        {/* <Head>
         <title>Empowering Data Analysis with Free - Data Watch</title>
       </Head> */}
-      <GoogleAnalytics trackPageViews={{ ignoreHashChange: true }} />
-      {getLayout(<Component {...pageProps} />)}
-      <DefaultSeo {...SEO_CONFIG} />
-      <ReactQueryDevtools initialIsOpen={false} />
+        <GoogleAnalytics trackPageViews={{ ignoreHashChange: true }} />
+        {getLayout(<Component {...pageProps} />)}
+        <Toaster containerClassName="react-toast" toastOptions={{ className: 'react-toast-el' }} />
+        <DefaultSeo {...SEO_CONFIG} />
+        <ReactQueryDevtools initialIsOpen={false} />
+      </Hydrate>
     </QueryClientProvider>
   );
 }
