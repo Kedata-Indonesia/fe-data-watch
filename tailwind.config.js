1663d66a7fc3ed48e06d954df317538d5fb1aea1
Bagus TRIANURDIN <btstargroup@gmail.com>
2023-07-31 01:36:10 +0700

    fix(tailwind.config): fix type module & add heading fontsize

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,5 +1,6 @@
-/** @type {import('tailwindcss').Config} */
 const defaultTheme = require('tailwindcss/defaultTheme');
+
+/** @type {import('tailwindcss').Config} */
 module.exports = {
   important: ['data-table-library_table'],
   content: [
@@ -26,6 +27,15 @@ module.exports = {
         'c-red-300': '#FF8098',
         'c-red-400': '#FF4C6F',
         'c-red-600': '#FF0031',
+        background: '#F5F6FA',
+      },
+      fontSize: {
+        'heading-6': ['1rem', '1.5rem'],
+        'heading-5': ['1.25rem', '1.75rem'],
+        'heading-4': ['1.5rem', '2rem'],
+        'heading-3': ['1.75rem', '2.25rem'],
+        'heading-2': ['2rem', '2.5rem'],
+        'heading-1': ['2.5rem', '3rem'],
       },
     },
   },
