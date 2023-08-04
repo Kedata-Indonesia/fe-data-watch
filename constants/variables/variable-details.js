b9fa0f243a0416b51588dc1f40ddf066557161e0
Bagus TRIANURDIN <btstargroup@gmail.com>
2023-08-04 16:39:26 +0700

    feat(constants/variables): add variable constants

---
+++ constants/variables/variable-details.js
@@ -0,0 +1,11 @@
+const VARIABLE_DETAILS = /** @type {const} */ ({
+  STATISTIC: 'statistic',
+  HISTOGRAM: 'histogram',
+  COMMON_VALUES: 'common-values',
+  EXTREME_VALUES: 'extreme-values',
+  OVERVIEW: 'overview',
+  WORDS: 'words',
+  CATEGORIES: 'categories',
+});
+
+export default VARIABLE_DETAILS;
---
+++ constants/variables/variable-types.js
@@ -0,0 +1,9 @@
+const VARIABLE_TYPES = /** @type {const} */ ({
+  NUMERIC: 'Numeric',
+  CATEGORICAL: 'Categorical',
+  DATE: 'Date',
+  TEXT: 'Text',
+  BOOLEAN: 'Boolean',
+});
+
+export default VARIABLE_TYPES;
