fc3c9c730e6d91daf539c1b158c496f979c42e27
Bagus TRIANURDIN <btstargroup@gmail.com>
2023-08-07 01:02:15 +0700

    fix(base/table): remove loading for temporary

--- components/base/table/table.js
+++ components/base/table/table.js
@@ -1,8 +1,9 @@
 import { CompactTable } from '@table-library/react-table-library/compact';
 import { useTheme } from '@table-library/react-table-library/theme';
 import { getTheme } from '@table-library/react-table-library/baseline';
+import { memo } from 'react';
 
-const Table = ({ columns = [], data = [], loading = false }) => {
+const Table = ({ columns = [], data = [] }) => {
   const tableData = { nodes: data };
 
   const theme = useTheme([
@@ -13,16 +14,12 @@ const Table = ({ columns = [], data = [], loading = false }) => {
         & [data-table-library_td] {
           min-width: 150px;
         }
-        &:nth-child(odd) {
+        &:nth-of-type(odd) {
           background-color: #F5F6FA;
         }
       `,
     },
   ]);
-
-  if (loading) {
-    return <Table.Skeleton />;
-  }
 
   return (
     <div className="h-full w-full">
@@ -35,33 +32,9 @@ const Table = ({ columns = [], data = [], loading = false }) => {
           fixedHeader: true,
           horizontalScroll: true,
         }}
-        resize={true}
       />
     </div>
   );
 };
 
-Table.Skeleton = () => {
-  return (
-    <div className="h-full w-full overflow-auto">
-      <div className="grid w-[70%] grid-cols-6 bg-white">
-        {Array.from({ length: 6 }).map((_, idx) => (
-          <div key={`tbl-skl-${idx}`} className="border-b border-r border-gray-300 bg-gray-100 p-3">
-            <div className="h-[20px] w-full animate-pulse rounded-sm bg-gray-300" />
-          </div>
-        ))}
-      </div>
-      <div className="grid w-[70%] grid-cols-6 bg-white">
-        {Array.from({ length: 102 }).map((_, idx) => (
-          <div key={`tbl-sklr-${idx}`} className="border-b border-r border-gray-300 p-3">
-            <div className="h-[20px] w-full animate-pulse rounded-sm bg-gray-300" />
-          </div>
-        ))}
-      </div>
-    </div>
-  );
-};
-
-Table.Skeleton.displayName = 'TableSkeleton';
-
-export default Table;
+export default memo(Table);
