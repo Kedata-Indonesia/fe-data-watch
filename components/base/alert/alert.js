ad174d7a6c22f26ccc2fde0613f23dfeec4fe33c
Bagus TRIANURDIN <btstargroup@gmail.com>
2023-08-10 15:01:27 +0700

    not completed

--- components/base/alert/alert.js
+++ components/base/alert/alert.js
@@ -1,7 +1,8 @@
 import withReactContent from 'sweetalert2-react-content';
 import Swal from 'sweetalert2';
+import ReportProblem from '@/components/icons/ReportProblem';
 
-const Alert = () => {
+const Alert = {
   /**
    * @param {object} props
    * @param {string} props.title
@@ -14,8 +15,9 @@ const Alert = () => {
       title: title || 'Error',
       text: text,
       icon: 'error',
+      iconHtml: <ReportProblem className="w-36 h-36" />,
     });
-  };
+  },
 };
 
 export default Alert;
