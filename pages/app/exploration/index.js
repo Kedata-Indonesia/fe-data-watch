499c9d8733234f523d411459be26dd95ce8957f3
Bagus TRIANURDIN <btstargroup@gmail.com>
2023-08-16 14:04:03 +0700

    fix(pages/app): change alert message when session will be expired

--- pages/app/exploration/index.js
+++ pages/app/exploration/index.js
@@ -26,8 +26,8 @@ const ExplorationPage = props => {
 
       if (state === 300) {
         Alert.error({
-          title: 'Session expired in 5 minutes',
-          text: 'Please upload your data again, later',
+          title: 'Session will expire in 5 minutes.',
+          text: 'Your session is valid for 15 minutes. Please remember to re-upload your file to continue using DataWatch without interruption. Thank you!',
         });
       }
 
--- pages/app/table/index.js
+++ pages/app/table/index.js
@@ -35,8 +35,8 @@ const TablePage = props => {
 
       if (state === 300) {
         Alert.error({
-          title: 'Session expired in 5 minutes',
-          text: 'Please upload your data again, later',
+          title: 'Session will expire in 5 minutes.',
+          text: 'Your session is valid for 15 minutes. Please remember to re-upload your file to continue using DataWatch without interruption. Thank you!',
         });
       }
 
